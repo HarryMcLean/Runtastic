90d289ec85a26492d5e7b7ae237ee6503cc42e45
omanmclean <42905081+omanmclean@users.noreply.github.com>
2018-11-05 17:05:41 +1000

    Trying to add functionality to display map at users current location. At this stage, app crashes when "Start Workout" is selected.

--- app/src/main/java/com/runtastic/runtasticmodel/SideNavBar.java
+++ app/src/main/java/com/runtastic/runtasticmodel/SideNavBar.java
@@ -1,5 +1,6 @@
 package com.runtastic.runtasticmodel;
 
+import android.app.Dialog;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -10,14 +11,20 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 
 
 public class SideNavBar extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private FusedLocationProviderClient mFusedLocationClient;
+    private static final String TAG = "MainActivity";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
     private DrawerLayout drawer;
 
     @Override
@@ -49,10 +56,11 @@ public class SideNavBar extends AppCompatActivity implements NavigationView.OnNa
         //Intent openGPS = new Intent(this,GpsTracker.class);
         Fragment fragment = null;
 
-        switch(menuItem.getItemId()){
+        switch (menuItem.getItemId()) {
             case R.id.nav_record:
-                //startActivity(openGPS);
-                fragment = new WorkoutFragment();
+                //if(isServicesOK()==true) {
+                    fragment = new WorkoutFragment();
+                //}
                 break;
             case R.id.nav_statistics:
                 fragment = new StatisticsFragment();
@@ -71,7 +79,7 @@ public class SideNavBar extends AppCompatActivity implements NavigationView.OnNa
                 break;
         }
 
-        if (fragment != null){
+        if (fragment != null) {
             displaySelectedScreen(fragment);
         }
 
@@ -79,19 +87,42 @@ public class SideNavBar extends AppCompatActivity implements NavigationView.OnNa
         return true;
     }
 
-    private void displaySelectedScreen(Fragment fragment){
+    private void displaySelectedScreen(Fragment fragment) {
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.fragment_container, fragment);
         ft.commit();
     }
 
     @Override
-    public void onBackPressed(){
-        if(drawer.isDrawerOpen(GravityCompat.START)){
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
         super.onBackPressed();
     }
+
+
+
+    public boolean isServicesOK() {
+        Log.d(TAG, "isServicesOK: checking google services version");
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(SideNavBar.this);
+    if(available == ConnectionResult.SUCCESS){
+        // user can make map requests
+        Log.d(TAG, "Google play services is working");
+        return true;
+    }
+    else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+        // an error occurred but it is resolvable
+        Log.d(TAG, "A resolvable error occurred");
+        Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(SideNavBar.this, available, ERROR_DIALOG_REQUEST);
+        dialog.show();
+    } else{
+        Toast.makeText(this, "We can't make map requests", Toast.LENGTH_SHORT).show();
+    }
+    return false;
+    }
+
+
 }
